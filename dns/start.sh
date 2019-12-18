86d67848a58cdf17813ee006fab562712de7ed0c
atyronesmith <aasmith@redhat.com>
2019-12-18 14:17:08 -0500

    more automation

--- dns/start.sh
+++ dns/start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-CONTAINER_NAME="ipi-cordnds"
+OUTPUT_DIR="generated"
+
+CONTAINER_NAME="ipi-coredns"
 CLUSTER_DNS="192.168.111.3"
 PROJECT_DIR="/root/kni-ipi-deploy"
 
+ORIGIN='$ORIGIN'
+source "../settings.sh"
+
+mkdir -p "$OUTPUT_DIR"
+ 
+cat db.zone | envsubst > "${OUTPUT_DIR}"/db.zone
+cat db.reverse | envsubst > "${OUTPUT_DIR}"/db.reverse
+
 podman run -d --expose=53/udp --name "$CONTAINER_NAME" \
             -p "$CLUSTER_DNS:53:53" -p "$CLUSTER_DNS:53:53/udp" \
-            -v "$PROJECT_DIR/dns:/etc/coredns:z" coredns/coredns:latest \
+            -v "$PROJECT_DIR/dns/$OUTPUT_DIR:/etc/coredns:z" coredns/coredns:latest \
             -conf /etc/coredns/Corefile
