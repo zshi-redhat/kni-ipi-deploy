522fe05f2d538521266c605526e11fe2b888f185
Andrew Bays <andrew.bays@gmail.com>
2020-01-07 11:19:26 -0500

    Improved config generation for dnsmasq + coredns

--- dhcp/start.sh
+++ dhcp/start.sh
@@ -3,23 +3,23 @@
 OUTPUT_DIR="generated"
 
 CONTAINER_NAME="ipi-dnsmasq-bm"
-PROJECT_DIR="/root/kni-ipi-deploy"
+PROJECT_DIR="$HOME/kni-ipi-deploy"
 
 # shellcheck disable=SC1091
 source "../settings.sh"
 
-mkdir -p "$OUTPUT_DIR"/bm/etc
-mkdir -p "$OUTPUT_DIR"/bm/var
+mkdir -p "$OUTPUT_DIR"/bm/etc/dnsmasq.d
+mkdir -p "$OUTPUT_DIR"/bm/var/run
  
-envsubst < dnsmasq.conf.tmpl > "${OUTPUT_DIR}"/bm/etc/dnsmasq.conf
-envsubst < dnsmasq.hostsfile.tmpl > "${OUTPUT_DIR}"/bm/etc/dnsmasq.hostsfile
+envsubst < dnsmasq.conf.tmpl > "${OUTPUT_DIR}"/bm/etc/dnsmasq.d/dnsmasq.conf
+envsubst < dnsmasq.hostsfile.tmpl > "${OUTPUT_DIR}"/bm/etc/dnsmasq.d/dnsmasq.hostsfile
 
 CONTAINER_NAME="ipi-dnsmasq-bm"
 CONTAINER_IMAGE="quay.io/poseidon/dnsmasq"
 
 podman run -d --name "$CONTAINER_NAME" --net=host \
-            -v "$PROJECT_DIR/dhcp/bm/var/run:/var/run/dnsmasq:Z" \
-            -v "$PROJECT_DIR/dhcp/bm/etc/dnsmasq.d:/etc/dnsmasq.d:Z" \
+            -v "$PROJECT_DIR/dhcp/$OUTPUT_DIR/bm/var/run:/var/run/dnsmasq:Z" \
+            -v "$PROJECT_DIR/dhcp/$OUTPUT_DIR/bm/etc/dnsmasq.d:/etc/dnsmasq.d:Z" \
             --expose=53 --expose=53/udp --expose=67 --expose=67/udp --expose=69 \
             --expose=69/udp --cap-add=NET_ADMIN "$CONTAINER_IMAGE" \
             --conf-file=/etc/dnsmasq.d/dnsmasq.conf -u root -d -q
--- dns/start.sh
+++ dns/start.sh
@@ -3,17 +3,20 @@
 OUTPUT_DIR="generated"
 
 CONTAINER_NAME="ipi-coredns"
-PROJECT_DIR="/root/kni-ipi-deploy"
+PROJECT_DIR="$HOME/kni-ipi-deploy"
 
 # shellcheck disable=SC1091
 source "../settings.sh"
 
 mkdir -p "$OUTPUT_DIR"
- 
-envsubst < db.zone > "${OUTPUT_DIR}"/db.zone
-envsubst < db.reverse > "${OUTPUT_DIR}"/db.reverse
+
+envsubst < Corefile.tmpl > "${OUTPUT_DIR}"/Corefile
+# shellcheck disable=SC2016
+envsubst '${CLUSTER_DOMAIN} ${CLUSTER_NAME}' < db.zone.tmpl > "${OUTPUT_DIR}"/db.zone
+# shellcheck disable=SC2016
+envsubst '${CLUSTER_DOMAIN} ${CLUSTER_NAME}' < db.reverse.tmpl > "${OUTPUT_DIR}"/db.reverse
 
 podman run -d --expose=53/udp --name "$CONTAINER_NAME" \
-            -p "$DNS_VIP:53:53" -p "$CLUSTER_DNS:53:53/udp" \
+            -p "$DNS_VIP:53:53/tcp" -p "$DNS_VIP:53:53/udp" \
             -v "$PROJECT_DIR/dns/$OUTPUT_DIR:/etc/coredns:z" coredns/coredns:latest \
             -conf /etc/coredns/Corefile
