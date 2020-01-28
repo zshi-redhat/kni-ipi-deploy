2df8a74e653e059c1759113bf678b2e7e87d9f7a
Andrew Bays <andrew.bays@gmail.com>
2020-01-28 07:07:51 -0500

    Version selection improvements for preinstall

--- install/preinstall.sh
+++ install/preinstall.sh
@@ -10,13 +10,36 @@
 # 6. install/install.sh 
 
 # Latest stable 4.3 release as of check-in
-VERSION="4.3.0-0.nightly-2020-01-16-031402"
-RELEASE_IMAGE=$(curl -s https://openshift-release-artifacts.svc.ci.openshift.org/$VERSION/release.txt | grep 'Pull From: registry' | awk -F ' ' '{print $3}' | xargs)
+VERSION="4.3.0-0.nightly-2020-01-27-075019"
+
+if [[ -z "$VERSION" ]]; then
+    echo "No version selected, trying \"latest\"..."
+    VERSION=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp-dev-preview/latest/release.txt | grep 'Name:' | awk -F: '{print $2}' | xargs)
+fi
+
+# First try to find version in official mirror
+RELEASE_IMAGE_SOURCE="https://mirror.openshift.com/pub/openshift-v4/clients/ocp-dev-preview"
+RELEASE_IMAGE=$(curl -s $RELEASE_IMAGE_SOURCE/$VERSION/release.txt | grep 'Pull From: quay.io' | awk -F ' ' '{print $3}' | xargs)
+
+if [[ -z "$RELEASE_IMAGE" ]]; then
+    # Version not found in official mirror, so try CI repo
+    RELEASE_IMAGE_SOURCE="https://openshift-release-artifacts.svc.ci.openshift.org"
+    RELEASE_IMAGE=$(curl -s $RELEASE_IMAGE_SOURCE/$VERSION/release.txt | grep 'Pull From: registry' | awk -F ' ' '{print $3}' | xargs)
+fi
+
+if [[ -z "$RELEASE_IMAGE" ]]; then
+    echo "Unable to find release image for version $VERSION!"
+    exit 1;
+fi
+
+echo "Using version $VERSION from repo $RELEASE_IMAGE_SOURCE"
+
 CMD=openshift-baremetal-install
 PULL_SECRET=~/pull-secret.json
 EXTRACT_DIR=$(pwd)
+
 # Get the oc binary
-curl https://openshift-release-artifacts.svc.ci.openshift.org/$VERSION/openshift-client-linux-$VERSION.tar.gz | tar zxvf - oc
+curl $RELEASE_IMAGE_SOURCE/$VERSION/openshift-client-linux-$VERSION.tar.gz | tar zxvf - oc
 sudo cp ./oc /usr/local/bin/oc
 # Extract the baremetal installer
 oc adm release extract --registry-config "${PULL_SECRET}" --command=$CMD --to "${EXTRACT_DIR}" ${RELEASE_IMAGE}
@@ -29,12 +52,16 @@ export RHCOS_PATH=$(curl -s -S https://raw.githubusercontent.com/openshift/insta
 # Place the URL printed by the following command into your metal3-config.yaml
 # as the "rhcos_image_url" value if necessary
 IMAGE_URL="$RHCOS_PATH$RHCOS_URI"
-echo "RHCOS URI: $RHCOS_URI"
-echo "RHCOS IMAGE: $IMAGE_URL"
+
+if [[ -f "$HOME/metal3-config.yaml" ]]; then
+    sed -i 's@rhcos_image_url.*@rhcos_image_url: '"$IMAGE_URL"'@' "$HOME/metal3-config.yaml"
+    echo "$HOME/metal3-config.yaml \"rhcos_image_url\" updated!"
+else
+    printf "RHCOS IMAGE (place this in metal3-config.yaml as \"rhcos_image_url\"):\n $IMAGE_URL\n"
+fi
 
 # Set up the image cache to boost bootstrap performance
 IMAGE_FILE=$(echo "$RHCOS_URI" | rev | cut -d '.' -f 2- | rev)
-echo "IMAGE_FILE: $IMAGE_FILE"
 IMAGE_DIR="$HOME/image_cache/images/$IMAGE_FILE"
 
 mkdir -p "$IMAGE_DIR"
@@ -57,4 +84,3 @@ mkdir -p "$IMAGE_DIR"
     sudo podman run --name image_cache -p 172.22.0.1:80:80/tcp -v /home/"$USER"/image_cache:/usr/share/nginx/html:ro -d nginx
 
 ) || exit 1
-
